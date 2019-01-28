d453064374084a841ed55fe7cad9ab9ee2bda98e
Daniel Bermudez <dbermudez@coadmins-MacBook-Pro-3.local>
2019-01-28 16:36:30 -0500

    Logica

--- Challenge-grafos/grafo.py
+++ Challenge-grafos/grafo.py
@@ -1,7 +1,11 @@
+import lzma
+
 import prueba
 entrada = open("Eurotrip.txt")
-paises = [" "]
+paises = []
+paisesconRutas = []
 viajes =  []
+
 
 for linea in entrada:
     viaje = []
@@ -10,21 +14,71 @@ for linea in entrada:
     # procesar línea
     if(linea.find(",") == -1):
         paises.append(linea.strip())
+
     else:
+
+
         relacion =linea.split(',')
         origen = relacion[0]
         destino = relacion[1]
         peso = relacion[2]
-        viajes.append([paises.index(origen), paises.index(destino),int(peso.strip())])
+        paisesconRutas.append(origen)
+        paisesconRutas.append(destino)
+        viajes.append([paises.index(origen)+1, paises.index(destino)+1,int(peso.strip())])
 
 
+paisesconRutas = set(paisesconRutas)
 entrada.close()
 
-print(paises)
-print(viajes)
-prueba.floyd_warshall(len(viajes),viajes)
+# print(paises)
+
+# print(len(paises))
+resultado = prueba.floyd_warshall((len(paisesconRutas)),viajes)
+print(resultado)
+
+with open("rutas.xz", "wb") as f:
+    # f.write(prueba.floyd_warshall((len(paisesconRutas)),viajes)[0].encode('utf-8'))
+    with lzma.open(f, "w") as lzf:
+        for n in range(len(resultado)):
+            ruta = resultado[n].split(",")
+            rutasConNombres=[]
+            rutaConNombres = " "
+            for p in range(len(ruta)):
+                if(p != 2 and p < (len(ruta)-1)):
+
+                        rutaConNombres += (paises[(int(ruta[p])-1)]+",")
+                elif (p == (len(ruta)-1)):
+                    if(ruta[p] == str(100 )):
+                        rutaConNombres += ("No se encontro una ruta posible")
+                    else:
+                        rutaConNombres += (paises[(int(ruta[p])-1)])
+
+                else:
+                    rutaConNombres +=(ruta[p]+ ",")
 
 
+
+            rutasConNombres.append(rutaConNombres)
+            # print(rutaConNombres)
+            for n in range(len(rutaConNombres)):
+                 lzf.write((rutaConNombres[n] ).encode('utf-8'))
+
+
+
+with lzma.open("rutas.xz") as f:
+    file_content = f.read()
+
+print(file_content.decode('utf-8'))
+
+with open("paises.xz", "wb") as f:
+    # f.write(prueba.floyd_warshall((len(paisesconRutas)),viajes)[0].encode('utf-8'))
+    with lzma.open(f, "w") as lzf:
+        for n in range(len(paises)):
+            lzf.write((paises[n] + "\n").encode('utf-8'))
+
+with lzma.open("paises.xz") as f:
+    file_content = f.read()
+print(file_content.decode('utf-8'))
 
 
 
--- Challenge-grafos/prueba.py
+++ Challenge-grafos/prueba.py
@@ -3,6 +3,7 @@ from itertools import product
 
 
 def floyd_warshall(n, edge):
+    rutas =[]
 
     rn = range(n)
     dist = [[inf] * n for i in rn]
@@ -17,17 +18,31 @@ def floyd_warshall(n, edge):
         if dist[i][j] > sum_ik_kj:
             dist[i][j] = sum_ik_kj
             nxt[i][j]  = nxt[i][k]
-    print("pair     dist    path")
+    # print("pair     dist    path")
     for i, j in product(rn, repeat=2):
         if i != j:
+
             path = [i]
-            while path[-1] != j:
-                path.append(nxt[path[-1]][j])
-            print("%d → %d  %4d       %s"
-                  % (i + 1, j + 1, dist[i][j],
-                     ' → '.join(str(p + 1) for p in path)))
+            bandera = 0
+            while path[-1] != j  and bandera == 0:
+                ruta = []
+                if(len(path)> 10):
+                    rutas.append("%d,%d,%d,%s"
+                                % (i + 1, j + 1,0,100))
+                    # print("%d,%d,%4d,%s"
+                    #       % (i + 1, j + 1,0," no se encontro una ruta posible"))
+                    bandera = 1
+                else:
+                    path.append(nxt[path[-1]][j])
+
+            if bandera ==0:
+                rutas.append("%d,%d,%d,%s"
+                 % (i + 1, j + 1,dist[i][j],",".join(str(p + 1) for p in path)))
+                # print("%d,%d,%4d%s"
+                #       % (i + 1, j + 1,dist[i][j],",".join(str(p + 1) for p in path)))
 
 
+    return(rutas)
 
 
 
